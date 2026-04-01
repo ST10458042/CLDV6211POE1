2a76987a156506f5af776fbaa23a81bb527c9b7c
ST10458042 <161014243+ST10458042@users.noreply.github.com>
2026-04-01 14:32:54 +0200

    Added packages

--- obj/Debug/net10.0/EventEase.AssemblyInfo.cs
+++ obj/Debug/net10.0/EventEase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4089a34b5beaad58a48b559159585a03c39153d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
